b6b4827a5b17ec351d7e4f62cee7996a7e4878ad
rogama <rogama_87@hotmail.com>
2013-03-24 19:59:31 +0100

    2 Activity

---
+++ Asteroides/src/com/rogama/asteroides/AcercaDe.java
@@ -0,0 +1,11 @@
+package com.rogama.asteroides;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AcercaDe extends Activity{
+	public void onCreate(Bundle savedInstaceState) {
+		super.onCreate(savedInstaceState);
+		setContentView(R.layout.acercade);
+	}
+}
--- Asteroides/src/com/rogama/asteroides/Asteroides.java
+++ Asteroides/src/com/rogama/asteroides/Asteroides.java
@@ -2,7 +2,9 @@ package com.rogama.asteroides;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class Asteroides extends Activity {
 
@@ -12,7 +14,6 @@ public class Asteroides extends Activity {
         setContentView(R.layout.main);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -20,4 +21,8 @@ public class Asteroides extends Activity {
         return true;
     }
     
+    public void lanzarAcercaDe(View view) {
+		Intent i = new Intent(this,AcercaDe.class);
+		startActivity(i);
+	}
 }
