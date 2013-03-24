a3bbee8115d7bb987f31525c11291187687bf007
rogama <rogama_87@hotmail.com>
2013-03-24 20:22:53 +0100

    Ventana Acerca De, su evento para abrirla, y boton de salir

--- Asteroides/src/com/rogama/asteroides/Asteroides.java
+++ Asteroides/src/com/rogama/asteroides/Asteroides.java
@@ -5,13 +5,33 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Asteroides extends Activity {
 
+	private Button bAcercaDe;//Declaramos un boton con el nombre bAcercaDe
+	private Button bSalir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        bAcercaDe = (Button) findViewById(R.id.button3);//al boton arriba declarado le asignamos el de la vista, el correspondiente al texto acerca de
+        bAcercaDe.setOnClickListener(new OnClickListener() {//Le asignamos un listener al evento Onclick
+        	
+			public void onClick(View view) {//al hacer click vendra aqui y ejecutara lo que contiene este procedimiento
+				lanzarAcercaDe(null);//saltamos a la funcion abajo declarada
+				
+			}
+		});
+        bSalir = (Button) findViewById(R.id.button4);
+        bSalir.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				salir(null);	
+			}
+		});
     }
 
     @Override
@@ -24,5 +44,9 @@ public class Asteroides extends Activity {
     public void lanzarAcercaDe(View view) {
 		Intent i = new Intent(this,AcercaDe.class);
 		startActivity(i);
+	}
+    
+    public void salir(View view) {
+		finish();
 	}
 }
